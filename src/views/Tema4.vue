d30f1383e86f732bdb4e984f6eb1174e724b1731
Leoska89 <83103793+Leoska89@users.noreply.github.com>
2024-06-07 11:27:01 -0500

    up

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -16,7 +16,7 @@
           .fl-box3
             p.mb-0 Es un instrumento vital para el mejoramiento genético, en el cual, mediante una pistola de inseminar, se introduce semen dentro del aparato genital de la hembra en celo o manejada bajo protocolos hormonales; se utiliza para disminuir los tiempos y ser más productivos.         
 
-    .row.justify-content-center.align-items-center.mb-5
+    .row.justify-content-center.align-items-center
       .col-lg-8 
         p En este proceso, el semen se insemina en la hembra colocando una porción de este en forma recolectada o diluida en el cuello uterino o el útero, mediante métodos mecánicos y en las condiciones más higiénicas. Sus experimentos demostraron que el poder fertilizante reside en los espermatozoides y no en la porción líquida del semen.
         p La inseminación artificial no es simplemente un método novedoso para lograr la fecundación en las hembras. Es una poderosa herramienta empleada principalmente para la mejora del ganado. En la inseminación artificial, el germoplasma de los toros de calidad superior se puede utilizar de manera efectiva en diferentes lugares. Mediante la adopción de la inseminación artificial, habría una reducción considerable de las enfermedades genitales y no genitales en el ganado de la granja.
@@ -29,10 +29,168 @@
     Separador
     #4_1.titulo-segundo.color-acento-contenido
       h2 4.1	Elementos necesarios para la Inseminación Artificial (IA)
-    p 
-    p 
+
+    p El sector ganadero busca mejorar la productividad de carne, leche y rusticidad, por medio de los cruzamientos, lo que ha conllevado al deterioro de las líneas raciales, repercutiendo en la disminución de calidad y cantidad de producción, e influyendo directamente en la rentabilidad. Actualmente, los adelantos biotecnológicos proponen mejorar los niveles productivos de una empresa ganadera, a partir de la inseminación artificial. en donde se está manejando e introduciendo el mejoramiento genético, prácticas que incrementan el valor productivo y reproductivo de los bovinos, haciendo rentable el negocio ganadero y mejorando la competitividad del sector.
+    p.mb-5 Para realizar una práctica acorde y bajo todos los parámetros técnicos, los elementos básicos utilizados en la inseminación artificial se presentan en la siguiente infografía:
 
 
+    ImagenInfografica.color-acento-botones.mb-5
+      template(v-slot:imagen)
+        figure
+          img(src='@/assets/curso/temas/tema4/img-3.png', alt='Texto que describa la imagen')
+          
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="21%" y="35%" numero="1")
+        .h5.mb-2 Tanque de almacenamiento
+        p Es un tanque criogénico de nitrógeno líquido, su tamaño depende de la cantidad de pajillas que se necesiten almacenar. 	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="6.5%" y="65%" numero="2")
+        .h5.mb-2 Pajillas y tijeras o corta pajillas
+        p Las pajillas contienen el semen y puede ser de 0.25 – 0.5 cc y el corta pajillas se utiliza para un corte adecuado a la pajilla.	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="17.8%" y="79.5%" numero="3")
+        .h5.mb-2 Toallas de papel
+        p Cumplen funciones de protección de la pajilla después de retirar del tanque y para limpiar la vulva del animal.	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="34.7%" y="75.8%" numero="4")
+        .h5.mb-2 Pistola de inseminación artificial	 
+        p Se utiliza para poder introducir directamente en el punto blanco de la hembra.	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="41.5%" y="32.4%" numero="5")
+        .h5.mb-2 Pinzas para la  manipulación de la pajilla
+        p Necesarias para no tener quemaduras al momento de retirar del tanque de nitrógeno.	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="70.6%" y="73%" numero="6")
+        .h5.mb-2 Recipientes para descongelado
+        p Este recipiente se utiliza para descongelar la pajilla, cuenta con un termómetro.	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="83.4%" y="37.2%" numero="7")
+        .h5.mb-2 Guantes de palpación desechables
+        p Necesarios para tener BPM y para evitar cualquier enfermedad zoonótica.	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="70.6%" y="26.1%" numero="8")
+        .h5.mb-2 Lubricante
+        p Se utiliza para no causar dolencias o daño en el animal al momento de introducir la mano vía rectal.	 
+
+      .tarjeta.color-acento-botones.fnd-4.p-3(x="59.6%" y="53%" numero="9")
+        .h5.mb-2 Fundas sanitarias y fundas protectoras	
+        p Utilizadas para no introducir ningún patógeno en la parte interior del aparato reproductor de la hembra.	 
+
+    
+    Separador
+    #4_2.titulo-segundo.color-acento-contenido
+      h2 4.2 Métodos y pasos de Inseminación artificial para las especies de producción
+
+    p La IA en animales se desarrolló originalmente para controlar la propagación de enfermedades, evitando el transporte de animales con patógenos potenciales a otras unidades animales para el apareamiento e impidiendo el contacto físico entre individuos. El uso de diluyentes de semen que contienen antibióticos también ayudó a prevenir la transmisión de enfermedades bacterianas. Las ventajas y desventajas de la IA son las siguientes:       
+    .row.mb-5.d-flex.align-items-stretch
+      .col-lg-6
+        .crd_02.mb-5
+          .crd_02__icon.dan
+            img(src="@/assets/curso/temas/tema4/img-4.svg", alt="alt")          
+          .crd_02__txt
+            h6 Ventajas:
+            p.mb-0 -No se necesita detección de calor.
+            p.mb-0 Mejora la eficiencia reproductiva en hatos.
+            p.mb-0 Se utiliza en cualquier estado del ciclo estral.
+            p.mb-0 Reduce el tiempo entre partos.
+            p.mb-0 Induce el estro en vacas con problemas de quistes.
+            p.mb-0 Mejora el porcentaje de preñez.
+      
+      .col-lg-6
+        .crd_02.mb-5
+          .crd_02__icon.dan
+            img(src="@/assets/curso/temas/tema4/img-5.svg", alt="alt")          
+          .crd_02__txt
+            h6 Desventajas:
+            p.mb-0 Requiere operaciones bien entrenadas y equipo especial.
+            p.mb-0 Requiere más tiempo que los servicios naturales.
+            p.mb-0 La limpieza inadecuada de los instrumentos y las precarias  condiciones higiénicas puede conducir a una menor fertilidad.
+            p.mb-0 Si el semen del toro no se analiza adecuadamente, se incrementará la propagación de enfermedades genitales.
+
+    p A partir del avance en la comprensión de la fisiología ovárica en rumiantes en las últimas décadas, ha sido posible el diseño de nuevas estrategias farmacológicas para controlar la reproducción de las hembras. De este modo hoy es posible sincronizar o inducir la ovulación de manera muy precisa, tanto en hembras que están ciclando como aquellas que se encuentran en postparto. Sumado a esto, el grado de sincronización que se logra con estos tratamientos es tal que permite realizar la inseminación a un tiempo predeterminado sin necesidad de detectar el estro, es conocida como Inseminación Artificial a Tiempo Fijo (IATF). Mediante la IATF entonces es posible superar las dos limitantes principales que históricamente habían representado un obstáculo para la adopción de la inseminación artificial en vacas de cría: el anestro posparto y la detección de celo.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        .crd_03.mb-5
+          .crd_03__icon.dan
+            img(src="@/assets/curso/temas/tema4/img-6.svg", alt="alt")          
+          .crd_03__txt
+            p.mb-0 La IATF es una tecnología que mediante el control de la dinámica folicular y del cuerpo lúteo logra inducir o sincronizar la ovulación en un momento conocido, permitiendo inseminar un gran número de animales cada día sin necesidad de detectar el estro.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-6
+       p Los tratamientos farmacológicos para inducir la ovulación requieren la administración de progesterona durante 7 u 8 días; en la actualidad existen diferentes dispositivos intravaginales que contienen este fármaco. El desarrollo de estos dispositivos de silicona representó un avance tecnológico importante en el control de la fisiología ovárica.  En la siguiente figura se presenta un ejemplo para el manejo reproductivo en bovinos: 
+
+      .col-lg-6
+        figure
+          img(src="@/assets/curso/temas/tema4/img-7.png", alt="alt") 
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-10
+        .titulo-sexto.color-acento-contenido
+          h5 Figura 6.
+          span Ejemplo Programas de IATF     
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-12
+            figure.mb-5
+              img.mb-2(src='@/assets/curso/temas/tema4/img-8.svg')
+              figcaption Nota. Genética bovina (s.f.). Cómo se hace la IAFTv
+
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8 
+        p La inseminación artificial requiere una inversión considerable, por lo cual se espera que cuando se realice el procedimiento sea exitoso. Lo ideal es que el proceso lo haga un profesional o un técnico experto en el tema, pero se aclara que el encargado de la finca o el mayordomo también lo pueden hacer, siempre y cuando estén muy bien capacitados.
+
+        p La técnica consiste en la inyección de semen preservado mediante nitrógeno líquido directamente en el útero de la forma más cuidadosa, aséptica y oportuna, para asegurar una dosis seminal con motilidad y viabilidad suficientes para lograr una concepción.
+
+        p Los pasos para una inseminación en la hembra bovina se determinan con la elección de la hembra, para ello se requiere que se efectúen los siguientes pasos que se presentan en el siguiente recurso: 
+      .col-lg-4
+        figure.mb-5
+          img(src='@/assets/curso/temas/tema4/img-9.png')
+
+
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-4")
+      .tarjeta.color-acento-botones
+        .row.justify-content-center.mb-3
+            img(src='@/assets/curso/temas/tema4/img-10.jpg' alt='AvatarTop')
+        h6.text-center Inmovilización
+        p.text-center Inmovilización  del animal.
+
+      .tarjeta.color-acento-botones
+        .row.justify-content-center.mb-3
+            img(src='@/assets/curso/temas/tema4/img-11.jpg' alt='AvatarTop')
+        h6.text-center Preparación de instrumentos
+        p.text-center Preparación de todos los instrumentos a utilizar, verificando el registro de la vaca y la pajilla.
+
+
+      .tarjeta.color-acento-botones
+        .row.justify-content-center.mb-3
+            img(src='@/assets/curso/temas/tema4/img-12.jpg' alt='AvatarTop')
+        h6.text-center.mx-3 Retirar la pajilla del tanque
+        p.text-center.mx-3 Retiro de la pajilla del tanque, tener en cuenta el número de la canastilla, este procedimiento no debe durar más de 5 segundos.
+
+      .tarjeta.color-acento-botones
+        .row.justify-content-center.mb-3
+            img(src='@/assets/curso/temas/tema4/img-13.jpg' alt='AvatarTop')
+        h6.text-center Cubrimiento de la pajilla
+        p.text-center Cubrimiento de la pajilla de la luz con las toallas de papel e introducir en descongelado (35°C - 38°C) por 45 a 60 segundos, secar y cortar.
+
+      .tarjeta.color-acento-botones
+        .row.justify-content-center.mb-3
+            img(src='@/assets/curso/temas/tema4/img-14.jpg' alt='AvatarTop')
+        h6.text-center Armado de la pistola
+        p.text-center Armado de la pistola con la pajilla, tener en cuenta que debe estar ajustada la funda sanitaria.
+
+
+      .tarjeta.color-acento-botones
+        .row.justify-content-center.mb-3
+            img(src='@/assets/curso/temas/tema4/img-15.jpg' alt='AvatarTop')
+        h6.text-center Aplicación del semen
+        p.text-center.text-small Ubicación de la vaca, limpieza de la vulva e introducción de  la mano vía rectal para ubicar el cérvix y ahí sí deslizar la pistola por el oz del cérvix, pasar los anillos y empujar el émbolo de la pistola lentamente para que el semen caiga en el cuerpo uterino.
+
+
+
+    p Para concluir se puede decir que la inseminación artificial ha tenido una gran importancia en el mejoramiento genético de los animales, especialmente en el ganado bovino, donde su práctica es un requisito indispensable para los altos requerimientos de producción en un corto período de tiempo.     
 
 </template>
 
