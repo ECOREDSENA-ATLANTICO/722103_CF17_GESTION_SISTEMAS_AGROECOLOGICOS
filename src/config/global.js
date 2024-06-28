d2ddfef9441eb6e21646de740097b61b2748ac62
Leoska89 <leoska890@gmail.com>
2024-06-28 15:32:47 -0500

    empaquetado

--- src/config/global.js
+++ src/config/global.js
@@ -218,7 +218,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/722103_CFA17_DU.pdf',
       },
       {
         icono: 'fas fa-download',
