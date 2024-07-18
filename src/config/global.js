cd2f9690a79ee70d9b171f9c594bd360dbedb045
AndresDiazPinto <andresvis565@gmail.com>
2024-07-18 10:26:23 -0500

    AJUSTES
    Se ajustan link de video, se actualiza CFA, se empaqueta CF

--- src/config/global.js
+++ src/config/global.js
@@ -218,7 +218,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/722103_CFA17_DU.zip',
+        download: 'downloads/722103_CFA17_DU.pdf',
       },
       {
         icono: 'fas fa-download',
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -23,7 +23,7 @@
 
         figure.mt-5(data-aos="fade-right")
           .video
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/UgmFdUgf5E8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="853" height="480" src="https://www.youtube.com/embed/kKAPZDgV5-8" title="Reproducción Pecuaria con Criterios Agroecológicos" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 
 </template>
